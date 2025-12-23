711a6ee1519d41e2ef41f1f4026624d5b39a1d58
David Dasenbrook <david.dasenbrook@tngtech.com>
2025-12-23 14:53:47 +0100

    feat: switch back to local upload provider

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,24 +1,3 @@
 export default ({ env }) => ({
-    upload: {
-    config: {
-      provider: 'aws-s3',
-      providerOptions: {
-        credentials: {
-          accessKeyId: env('HETZNER_ACCESS_KEY_ID'),
-          secretAccessKey: env('HETZNER_ACCESS_SECRET'),
-        },
-        region: env('HETZNER_REGION'),
-        baseUrl: `https://${env('HETZNER_BUCKET')}.${env('HETZNER_REGION')}.${env('HETZNER_BASE_URL')}`,
-        endpoint: `https://${env('HETZNER_REGION')}.${env('HETZNER_BASE_URL')}`,
-        params: {
-          Bucket: env('HETZNER_BUCKET'),
-        },
-      },
-      actionOptions: {
-        upload: {},
-        uploadStream: {},
-        delete: {},
-      },
-    },
-  },
+
 });
