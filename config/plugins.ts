d7de125ceda0da01e94b1609e01a890821ddbe5b
David Dasenbrook <david.dasenbrook@gmail.com>
2026-01-25 12:35:27 +0100

    feat: enable caching for file uploads

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,3 +1,11 @@
 export default ({ env }) => ({
-
+upload: {
+    config: {
+      providerOptions: {
+        localServer: {
+          maxage: 604800, // 7 days in seconds
+        },
+      },
+    },
+  },
 });
